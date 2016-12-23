37ea682b5fb36d1841bcf6bd871aadb5b4de5d39
Rok Garbas <rok@garbas.si>
2016-12-23 17:34:04 +0100

    allowing update to accept custom version of pypi2nix
    also package with python 3.5 (3.4 is already deprecated)

--- update.nix
+++ update.nix
@@ -1,23 +1,30 @@
 { pkgs ? import (builtins.fetchTarball "https://d3g5gsiof5omrk.cloudfront.net/nixos/16.09/nixos-16.09.1324.1dd0fb6/nixexprs.tar.xz") {}
+, pypi2nix ? null
 }:
 
-pkgs.stdenv.mkDerivation {
+let
+  pypi2nix' =
+    if pypi2nix == null
+      then "${pkgs.pypi2nix}/bin/pypi2nix"
+      else pypi2nix;
+in pkgs.stdenv.mkDerivation {
   name = "update-vulnix";
   buildCommand = ''
     echo "+--------------------------------------------------------+"
-    echo "| Not possible to update repositories using \`nix-build\`. |"
+    echo "| Not possible to update vulnix using \`nix-build\`. |"
     echo "|         Please run \`nix-shell update.nix\`.             |"
     echo "+--------------------------------------------------------+"
     exit 1
   '';
   shellHook = ''
     export HOME=$PWD
-    ${pkgs.pypi2nix}/bin/pypi2nix -V 3.4 \
+    ${pypi2nix'} -V 3.5 \
             -b buildout.cfg \
             -E "libxml2 libxslt" \
             -e pytest-runner \
             -e setuptools-scm \
             -v
-    echo "Packages updated!"
+    echo "Vulnix updated!"
+    exit 0
   '';
 }
